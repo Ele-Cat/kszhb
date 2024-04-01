b4f43524f5b51e4ad3ba179d52c38f01467c46f4
ele-cat <951572198@qq.com>
2024-04-01 20:31:37 +0800

    完善

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import App from './App'
-import store from './store'
 
 // #ifndef VUE3
 import Vue from 'vue'
@@ -13,7 +12,6 @@ Vue.prototype.$backgroundAudioData = {
 }
 App.mpType = 'app'
 const app = new Vue({
-	store,
 	...App
 })
 app.$mount()
@@ -25,7 +23,6 @@ import {
 } from 'vue'
 export function createApp() {
 	const app = createSSRApp(App)
-	app.use(store)
 	app.config.globalProperties.$adpid = "1111111111"
 	app.config.globalProperties.$backgroundAudioData = {
 		playing: false,
