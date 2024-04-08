5cc0d2a6f14d332cce3a2d097e7afe7c2138d3e2
ele-cat <951572198@qq.com>
2024-04-08 21:32:05 +0800

    完善

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
-Vue.prototype.$store = store
-Vue.prototype.$adpid = "1111111111"
 Vue.prototype.$backgroundAudioData = {
 	playing: false,
 	playTime: 0,
